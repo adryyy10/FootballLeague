e42ca85471694dc61efbe73c53927aeaf88ae43c
adryyy10 <adriafigueresgarcia@gmail.com>
2022-09-10 17:33:38 +0200

    New button added in order to create new coach + form

--- src/Infrastructure/Controllers/CoachController.php
+++ src/Infrastructure/Controllers/CoachController.php
@@ -13,7 +13,7 @@ class CoachController extends AbstractController
     /**
      * @Route("/coaches", name="app_coach")
      */
-    public function listingCoaches(ManagerRegistry $managerRegistry, QueryHandler $useCase): Response
+    public function listingCoaches(QueryHandler $useCase): Response
     {
         
         $response = $useCase();
@@ -21,6 +21,18 @@ class CoachController extends AbstractController
         return $this->render('coach/index.html.twig', [
             'controller_name'   => 'CoachController',
             'coaches'           => $response->getCoaches()
+        ]);
+    }
+
+    /**
+     * @Route("/addCoach", name="app_add_coach")
+     */
+    public function addCoach()
+    {
+        $test = 1;
+        return $this->render('coach/add--coach.html.twig', [
+            'controller_name'   => 'CoachController',
+            'test'              => $test
         ]);
     }
 }
