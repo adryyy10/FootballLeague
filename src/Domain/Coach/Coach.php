e552a45f8ebadc9013e559c4d24f70e2a60424ae
adryyy10 <adriafigueresgarcia@gmail.com>
2022-10-16 09:16:10 +0200

    modified salaryException

--- src/Domain/Coach/Coach.php
+++ src/Domain/Coach/Coach.php
@@ -5,6 +5,7 @@ namespace App\Domain\Coach;
 use Doctrine\ORM\Mapping as ORM;
 use App\Domain\Club\Club;
 use App\Domain\Exceptions\EmptyCoachNameException;
+use App\Domain\Exceptions\EmptySalaryException;
 
 /**
  * @ORM\Entity(repositoryClass=CoachRepository::class)
